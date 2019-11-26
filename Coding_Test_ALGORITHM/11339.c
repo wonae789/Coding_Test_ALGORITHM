2d1cc7debe0d5bc2e0fb45b4cd814c4dca4e7f90
wonae789 <wonae789@naver.com>
2019-11-27 07:34:32 +0900

    ATM 대기시간

---
+++ Coding_Test_ALGORITHM/11339.c
@@ -0,0 +1,52 @@
+//https://www.acmicpc.net/problem/11399
+//ATM 대기시간
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+BubbleSort(int* arry, int getnum)
+{
+	int temp;
+	for (int i = 0; i < getnum - 1; i++)
+	{
+		for (int j = 0; j < (getnum - i) - 1; j++)
+		{
+			if (arry[j] > arry[j + 1])
+			{
+				temp = arry[j];
+				arry[j] = arry[j + 1];
+				arry[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int* arry;
+	int getnum;
+
+	int sum = 0;
+	scanf("%d", &getnum);
+	arry = (int*)malloc(sizeof(int) * getnum);
+
+
+	for (int i = 0; i < getnum; i++)
+	{
+		scanf("%d", &arry[i]);
+	}
+
+
+	BubbleSort(arry, getnum);
+
+
+	for (int i = 0; i < getnum; i++)
+	{
+		for (int k = 0; k <= i; k++)
+		{
+			sum += arry[k];
+		}
+	}
+
+	printf("%d", sum);
+	return 0;
+}
